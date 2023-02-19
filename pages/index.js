fef4696e94980b6e7e32d6ea3dbe963167ef491e
Harry Arkwright <84912003+Harry-Arkwright@users.noreply.github.com>
2023-02-19 14:25:31 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -65,13 +65,13 @@ const callGenerateEndpoint = async () => {
 
 
   <div  className="whatsapp-link">
-<a href="whatsapp://send?text=Your own personal coach:    ">
- 
-<a className="whatsapp-link">
-  href="https://twitter.com/intent/tweet?text=Hello%20world">
-Tweet> Share Your Advice</a>
-</div>
+<a href="whatsapp://send?text=Your own personal coach:    ">Share your advice</a>
+ </div>
 
+  <div  className="whatsapp-link">
+<a href="https://twitter.com/intent/tweet?text=Hello%20world">Tweet your advice</a>
+
+</div>
 
   {apiOutput && (
   <div className="output">
